d4891c67f483f1d96969cdf928585734287f9b2f
jela3105 <jela3105@gmail.com>
2021-02-07 18:05:28 -0600

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:designs/src/pages/basic_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Designs',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'basic',
+      routes: {
+        'basic': (BuildContext context) => BasicPage(),
+      },
+    );
+  }
+}
