433976eacc8dbba46771b45ecee4139a8e5ad28d
jela3105 <jela3105@gmail.com>
2021-02-10 22:54:45 -0600

    Create pageview with vertical direction

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ScrollPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello world'),
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          _page1(),
+          _page2(),
+        ],
       ),
+    );
+  }
+
+  Widget _page1() {
+    return Container(
+      child: Text('Page1'),
+    );
+  }
+
+  Widget _page2() {
+    return Container(
+      child: Text('Page1'),
     );
   }
 }
