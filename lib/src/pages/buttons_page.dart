7a2d13a473f69ab933cc1388b3c38bc3c1b155c1
jela3105 <jela3105@gmail.com>
2021-02-15 23:25:13 -0600

    Create first gradient

--- lib/src/pages/buttons_page.dart
+++ lib/src/pages/buttons_page.dart
@@ -4,9 +4,34 @@ class ButtonsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello page'),
+      body: Stack(
+        children: <Widget>[
+          _backgroundApp(),
+        ],
       ),
+    );
+  }
+
+  Widget _backgroundApp() {
+    final gradient = Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset(0.0, 0.4),
+          end: FractionalOffset(0.0, 1.0),
+          colors: [
+            Color.fromRGBO(52, 54, 101, 1.0),
+            Color.fromRGBO(35, 37, 57, 1.0),
+          ],
+        ),
+      ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        gradient,
+      ],
     );
   }
 }
