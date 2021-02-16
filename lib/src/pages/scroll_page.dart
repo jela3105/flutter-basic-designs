b44850c645c35a6bebdbd4cc9339219d8bbe1b97
jela3105 <jela3105@gmail.com>
2021-02-15 22:54:48 -0600

    Create fist widget in stack of page1

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -16,14 +16,33 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _page1() {
-    return Container(
-      child: Text('Page1'),
+    return Stack(
+      children: <Widget>[_backgroundColor(), _backgroundImage()],
     );
   }
 
   Widget _page2() {
     return Container(
       child: Text('Page1'),
+    );
+  }
+
+  Widget _backgroundColor() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1.0),
+    );
+  }
+
+  Widget _backgroundImage() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
